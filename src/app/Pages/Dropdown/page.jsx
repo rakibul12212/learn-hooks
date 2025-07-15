62eee726bde28f304d1132945c524be159e37221
rakibul-xeon <xeon.rakibul@gmail.com>
2025-07-15 19:21:51 +0600

    dropdown

--- src/app/Pages/Dropdown/page.jsx
+++ src/app/Pages/Dropdown/page.jsx
@@ -116,3 +116,100 @@ const DropdownPage = () => {
 };
 
 export default DropdownPage;
+
+
+
+
+// "use client";
+// import { useState } from "react";
+
+// const data = {
+//   countries: [
+//     {
+//       country: "Bangladesh",
+//       divisions: [
+//         {
+//           name: "Dhaka",
+//           districts: ["Gazipur", "Narayanganj", "Tangail"],
+//         },
+//         {
+//           name: "Chittagong",
+//           districts: ["Comilla", "Cox's Bazar", "Rangamati"],
+//         },
+//         {
+//           name: "Khulna",
+//           districts: ["Jessore", "Satkhira", "Bagerhat"],
+//         },
+//       ],
+//     },
+//     {
+//       country: "India",
+//       divisions: [
+//         {
+//           name: "Maharashtra",
+//           districts: ["Mumbai", "Pune", "Nagpur"],
+//         },
+//         {
+//           name: "West Bengal",
+//           districts: ["Kolkata", "Howrah", "Darjeeling"],
+//         },
+//         {
+//           name: "Punjab",
+//           districts: ["Amritsar", "Ludhiana", "Patiala"],
+//         },
+//       ],
+//     },
+//     {
+//       country: "USA",
+//       divisions: [
+//         {
+//           name: "California",
+//           districts: ["Los Angeles", "San Francisco", "San Diego"],
+//         },
+//         {
+//           name: "Texas",
+//           districts: ["Houston", "Dallas", "Austin"],
+//         },
+//         {
+//           name: "Florida",
+//           districts: ["Miami", "Orlando", "Tampa"],
+//         },
+//       ],
+//     },
+//   ],
+// };
+
+// const DropdownPage = () => {
+//   return (
+//     <div className="p-20 gap-4 flex justify-center">
+//       <select className="border-2 rounded-md p-2 w-full">
+//         <option value="">Select a country</option>
+//         {data.countries.map((country, index) => (
+//           <option key={index} value={country.country}>
+//             {country.country}
+//           </option>
+//         ))}
+//       </select>
+
+//       <select className="border-2 rounded-md p-2 w-full">
+//         <option value="">Select a division</option>
+//         {data.countries[0]?.divisions?.map((division, index) => (
+//           <option key={index} value={division.name}>
+//             {division.name}
+//           </option>
+//         ))}
+//       </select>
+//       <select className="border-2 rounded-md p-2 w-full">
+//         <option value="">Select a division</option>
+//         {data.countries[0]?.divisions[0]?.districts?.map((district, index) => (
+//           <option key={index} value={district.name}>
+//             {district.name}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//   );
+// };
+
+// export default DropdownPage;
+
